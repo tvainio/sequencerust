dbdb356790ee149d3acfbf008489169c03d3d055
Teemu Vainio <tvainiotheprogrammer@gmail.com>
2019-07-26 10:25:06 +0300

    Created sequencer struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,27 @@
+pub struct Sequencer {
+    current_step: u8,
+}
+
+impl Sequencer {
+    fn new() -> Sequencer {
+        Sequencer { current_step: 0 }
+    }
+}
+
+pub fn tick(s: Sequencer) -> Sequencer {
+    Sequencer {
+        current_step: s.current_step + 1,
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn it_ticks() {
+        let s = Sequencer::new();
+        let s2 = tick(s);
+        assert_eq!(s2.current_step, 1);
     }
 }
