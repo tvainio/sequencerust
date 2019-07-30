e769d89aa5b25575b63764ce64ccdcfbfd03b3e2
Teemu Vainio <tvainiotheprogrammer@gmail.com>
2019-07-30 21:58:24 +0300

    sequencer only ticks in active steps

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,25 @@
-/*
-
-pub struct Sequencer<F> where F:Fn() {
-    current_step: u32,
-    callback: F
-}
- 
-impl Sequencer<Fn()> {
-    fn new<F>(f: F) -> Sequencer<F> {
-        Sequencer { current_step: 0, callback: f }
-    }
-}
-
-pub fn tick(s: Sequencer<) -> Sequencer<F> {
-    Sequencer {
-        current_step: (s.current_step + 1) % PATTERN_LENGTH,
-    }
-}
-
- */
-const PATTERN_LENGTH:u32 = 16;
+const PATTERN_LENGTH:usize = 16;
 
 pub struct Sequencer<CB> where CB: FnMut() {
-    current_step: u32,
+    current_step: usize,
     callback: CB,
+    steps: Vec<bool>
 }
 
 impl<CB> Sequencer<CB> where CB: FnMut() {
     pub fn new(cb: CB)->Sequencer<CB> {
-        Sequencer {current_step: 0, callback:cb}
+        Sequencer {current_step: 0, callback:cb, steps:vec![false; PATTERN_LENGTH]}
     }
 
     pub fn tick(&mut self) {
+        if self.steps[self.current_step]==true {
+            (self.callback)();
+        }
         self.current_step = (self.current_step + 1) % PATTERN_LENGTH;
-        (self.callback)();
+    }
+
+    pub fn set(&mut self, step:usize, state:bool) {
+        self.steps[step] = state;
     }
 }
 
@@ -57,10 +44,20 @@ mod tests {
     }
 
     #[test]
-    fn it_triggers() {
+    fn it_doesnt_trigger_if_step_is_disabled() {
         let mut called = false;
         let cb = ||called=true;
-        let mut s = Sequencer {current_step: 0, callback:cb};
+        let mut s = Sequencer::new(cb);
+        s.tick();
+        assert_eq!(s.current_step, 1);
+        assert_eq!(called, false);
+    }
+    #[test]
+    fn it_triggers_only_if_active_step() {
+        let mut called = false;
+        let cb = ||called=true;
+        let mut s = Sequencer::new(cb);
+        s.set(0, true);
         s.tick();
         assert_eq!(s.current_step, 1);
         assert_eq!(called, true);
